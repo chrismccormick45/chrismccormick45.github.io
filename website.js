2e3fcd24448d426e7495e63f048bf711bd7646a3
chrismccormick45 <108166449+chrismccormick45@users.noreply.github.com>
2023-04-01 16:55:05 -0700

    Add files via upload

--- website.js
+++ website.js
@@ -1,31 +1,73 @@
 
 
-let slideIndex = 1;
-showSlides(slideIndex);
+/*
+var slideshow-planes = document.getElementById("slideshow-planes");
+slideshow-planes.currentSlideIndex = 1;
+showSlides(slideshow-planes.currentSlideIndex, slideshow_planes);
+
+var slideshow_rockets = document.getElementById("slideshow_rockets");
+slideshow_rockets.currentSlideIndex = 1;
+showSlides(slideshow_rockets.currentSlideIndex, slideshow_rockets);
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideshow.currentSlideIndex += n, slideshow);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideshow.currentSlideIndex = n, slideshow);
 }
 
-function showSlides(n) {
+function showSlides(n, slideshow) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideshow.CurrentSlideIndex = 1 }
+    if (n < 1) { slideshow.currentSlideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    slides[slideshow.currentSlideIndex - 1].style.display = "block";
+    dots[slideshow.currentSlideIndex - 1].className += " active";
+} */
+
+var slideshow1 = document.getElementById("slideshow1");
+slideshow1.currentSlideIndex = 1;
+showSlides(slideshow1.currentSlideIndex, slideshow1);
+
+/*
+var slideshow2 = document.getElementById("slideshow2");
+slideshow2.currentSlideIndex = 1;
+showSlides(slideshow2.currentSlideIndex, slideshow2);
+*/
+
+function plusSlides(n, slideshow) {
+    showSlides(slideshow.currentSlideIndex += n, slideshow);
 }
 
+function currentSlide(n, slideshow) {
+    showSlides(slideshow.currentSlideIndex = n, slideshow);
+}
+
+function showSlides(n, slideshow) {
+
+
+
+    var i;
+    var slides = slideshow.getElementsByClassName("mySlides");
+    var dots = slideshow.getElementsByClassName("dot");
+    if (n > slides.length) { slideshow.currentSlideIndex = 1 }
+    if (n < 1) { slideshow.currentSlideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideshow.currentSlideIndex - 1].style.display = "block";
+    dots[slideshow.currentSlideIndex - 1].className += " active";
+}
