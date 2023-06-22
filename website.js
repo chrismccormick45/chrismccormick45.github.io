097560a5beeaf3c1b4530208d4214270686ab37e
chrismccormick45 <108166449+chrismccormick45@users.noreply.github.com>
2023-06-21 22:38:25 -0700

    Add files via upload

--- website.js
+++ website.js
@@ -1,73 +1,51 @@
 
+// Making slideshow variables
+var slideShow_E96Planes = document.getElementById("slideShow_E96Planes");
+slideShow_E96Planes.currentSlideIndex = 1;
+showSlides(slideShow_E96Planes.currentSlideIndex, slideShow_E96Planes);
 
-/*
-var slideshow-planes = document.getElementById("slideshow-planes");
-slideshow-planes.currentSlideIndex = 1;
-showSlides(slideshow-planes.currentSlideIndex, slideshow_planes);
+var slideShow_E96Rockets = document.getElementById("slideShow_E96Rockets");
+slideShow_E96Rockets.currentSlideIndex = 1;
+showSlides(slideShow_E96Rockets.currentSlideIndex, slideShow_E96Rockets);
 
-var slideshow_rockets = document.getElementById("slideshow_rockets");
-slideshow_rockets.currentSlideIndex = 1;
-showSlides(slideshow_rockets.currentSlideIndex, slideshow_rockets);
-
-// Next/previous controls
-function plusSlides(n) {
+// Functions to progress slide show
+function plusSlides(n, slideshow)
+{
     showSlides(slideshow.currentSlideIndex += n, slideshow);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
+function currentSlide(n, slideshow)
+{
     showSlides(slideshow.currentSlideIndex = n, slideshow);
 }
 
-function showSlides(n, slideshow) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideshow.CurrentSlideIndex = 1 }
-    if (n < 1) { slideshow.currentSlideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideshow.currentSlideIndex - 1].style.display = "block";
-    dots[slideshow.currentSlideIndex - 1].className += " active";
-} */
-
-var slideshow1 = document.getElementById("slideshow1");
-slideshow1.currentSlideIndex = 1;
-showSlides(slideshow1.currentSlideIndex, slideshow1);
-
-/*
-var slideshow2 = document.getElementById("slideshow2");
-slideshow2.currentSlideIndex = 1;
-showSlides(slideshow2.currentSlideIndex, slideshow2);
-*/
-
-function plusSlides(n, slideshow) {
-    showSlides(slideshow.currentSlideIndex += n, slideshow);
-}
-
-function currentSlide(n, slideshow) {
-    showSlides(slideshow.currentSlideIndex = n, slideshow);
-}
-
-function showSlides(n, slideshow) {
-
-
-
+// Function to display images and videos in slideshow
+function showSlides(n, slideshow)
+{
     var i;
     var slides = slideshow.getElementsByClassName("mySlides");
     var dots = slideshow.getElementsByClassName("dot");
-    if (n > slides.length) { slideshow.currentSlideIndex = 1 }
-    if (n < 1) { slideshow.currentSlideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
+
+    if (n > slides.length)
+    {
+        slideshow.currentSlideIndex = 1
+    }
+
+    if (n < 1)
+    {
+        slideshow.currentSlideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++)
+    {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+
+    for (i = 0; i < dots.length; i++)
+    {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+
     slides[slideshow.currentSlideIndex - 1].style.display = "block";
     dots[slideshow.currentSlideIndex - 1].className += " active";
 }
