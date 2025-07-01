75323e0f7027c2ea5abcc90fb43864dda3858755
chrismccormick45 <108166449+chrismccormick45@users.noreply.github.com>
2025-06-30 17:35:02 -0700

    Add files via upload

--- website.js
+++ website.js
@@ -1,20 +1,45 @@
 
 // Making slideshow variables
-var slideShow_Taira = document.getElementById("slideShow_Taira");
-slideShow_Taira.currentSlideIndex = 1;
-showSlides(slideShow_Taira.currentSlideIndex, slideShow_Taira);
+
+var slideShow_About = document.getElementById("slideShow_About");
+slideShow_About.currentSlideIndex = 1;
+showSlides(slideShow_About.currentSlideIndex, slideShow_About);
+
+var slideShow_TairaLab = document.getElementById("slideShow_TairaLab");
+slideShow_TairaLab.currentSlideIndex = 1;
+showSlides(slideShow_TairaLab.currentSlideIndex, slideShow_TairaLab);
+
+var slideShow_Mayfly = document.getElementById("slideShow_Mayfly");
+slideShow_Mayfly.currentSlideIndex = 1;
+showSlides(slideShow_Mayfly.currentSlideIndex, slideShow_Mayfly);
+
+var slideShow_SilentNightSurveillant = document.getElementById("slideShow_SilentNightSurveillant");
+slideShow_SilentNightSurveillant.currentSlideIndex = 1;
+showSlides(slideShow_SilentNightSurveillant.currentSlideIndex, slideShow_SilentNightSurveillant);
+
+var slideShow_AutonomousDrone = document.getElementById("slideShow_AutonomousDrone");
+slideShow_AutonomousDrone.currentSlideIndex = 1;
+showSlides(slideShow_AutonomousDrone.currentSlideIndex, slideShow_AutonomousDrone);
+
+var slideShow_CFD = document.getElementById("slideShow_CFD");
+slideShow_CFD.currentSlideIndex = 1;
+showSlides(slideShow_CFD.currentSlideIndex, slideShow_CFD);
+
+var slideShow_Misc = document.getElementById("slideShow_Misc");
+slideShow_Misc.currentSlideIndex = 1;
+showSlides(slideShow_Misc.currentSlideIndex, slideShow_Misc);
 
 var slideShow_Drone = document.getElementById("slideShow_Drone");
 slideShow_Drone.currentSlideIndex = 1;
 showSlides(slideShow_Drone.currentSlideIndex, slideShow_Drone);
 
-var slideShow_E96Planes = document.getElementById("slideShow_E96Planes");
-slideShow_E96Planes.currentSlideIndex = 1;
-showSlides(slideShow_E96Planes.currentSlideIndex, slideShow_E96Planes);
+var slideShow_Plane = document.getElementById("slideShow_Plane");
+slideShow_Plane.currentSlideIndex = 1;
+showSlides(slideShow_Plane.currentSlideIndex, slideShow_Plane);
 
-var slideShow_E96Rockets = document.getElementById("slideShow_E96Rockets");
-slideShow_E96Rockets.currentSlideIndex = 1;
-showSlides(slideShow_E96Rockets.currentSlideIndex, slideShow_E96Rockets);
+var slideShow_Rocket = document.getElementById("slideShow_Rocket");
+slideShow_Rocket.currentSlideIndex = 1;
+showSlides(slideShow_Rocket.currentSlideIndex, slideShow_Rocket);
 
 // Functions to progress slide show
 function plusSlides(n, slideshow)
